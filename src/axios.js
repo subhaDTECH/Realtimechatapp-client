b04927abbc99026e9cc31cfffc418a0457319002
subhaDTECH <76237973+subhaDTECH@users.noreply.github.com>
2023-01-10 04:36:53 +0530

    change

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 const instance=axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://realtime-chat-app-ar38.onrender.com"
 })
 export default instance;
